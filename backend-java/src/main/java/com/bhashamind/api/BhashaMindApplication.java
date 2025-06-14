2918e9c1b81be56eb63628f40dd46f6950c36759
Abhishek Maity <abhishekmaity@users.noreply.github.com>
2025-06-14 13:19:17 +0530

    BhashaMindApplication.java added

---
+++ backend-java/src/main/java/com/bhashamind/api/BhashaMindApplication.java
@@ -0,0 +1,6 @@
+@SpringBootApplication
+public class BhashaMindApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BhashaMindApplication.class, args);
+    }
+}
