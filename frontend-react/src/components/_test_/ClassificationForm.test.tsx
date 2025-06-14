079651a15c91641ebfed8ccb7d2ab16d7db7b072
Abhishek Maity <abhishekmaity@users.noreply.github.com>
2025-06-14 17:15:51 +0530

    Create ClassificationForm.test.tsx

---
+++ frontend-react/src/components/_test_/ClassificationForm.test.tsx
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import ClassificationForm from '../ClassificationForm';
+
+test('renders classification form input and button', () => {
+  render(<ClassificationForm />);
+  expect(screen.getByPlaceholderText(/এখানে টেক্সট দিন/i)).toBeInTheDocument();
+  expect(screen.getByRole('button', { name: /শ্রেণিবিন্যাস করুন/i })).toBeInTheDocument();
+});
