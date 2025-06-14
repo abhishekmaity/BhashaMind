979f122f50666c7cfb76606171d2253f4d767c34
Abhishek Maity <abhishekmaity@users.noreply.github.com>
2025-06-14 13:31:03 +0530

    Update preprocessing.py

---
+++ models/utils/preprocessing.py
@@ -0,0 +1,10 @@
+preprocessing.pyfrom datasets import load_dataset
+
+def load_bengali_dataset(task="summarization"):
+    if task == "summarization":
+        dataset = load_dataset("csebuetnlp/xlsum", "bn")
+    elif task == "classification":
+        dataset = load_dataset("ai4bharat/indic-classification", "bn")
+    else:
+        raise ValueError("Unknown task")
+    return dataset
