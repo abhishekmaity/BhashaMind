ae230913cb92ace9171340110b5732ca7d591ef9
Abhishek Maity <abhishekmaity@users.noreply.github.com>
2025-06-26 00:43:14 +0530

    Create SummarizationRequest.java

---
+++ backend-java/src/main/java/com/bhashamind/api/dto/SummarizationRequest.java
@@ -0,0 +1,7 @@
+package com.bhashamind.api.dto;
+
+public class SummarizationRequest {
+    private String text;
+    public String getText() { return text; }
+    public void setText(String text) { this.text = text; }
+}
