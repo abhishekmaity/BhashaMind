21f2d746e334be79cba24838999ac26e3189715f
Abhishek Maity <abhishekmaity@users.noreply.github.com>
2025-06-26 00:09:31 +0530

    Create producer.py

---
+++ backend-python/app/queue/producer.py
@@ -0,0 +1,14 @@
+# backend-python/app/queue/producer.py
+
+import json
+from app.queue.connection import get_connection
+
+async def send_task_to_queue(queue_name: str, payload: dict):
+    conn = await get_connection()
+    async with conn:
+        channel = await conn.channel()
+        queue = await channel.declare_queue(queue_name, durable=True)
+        await channel.default_exchange.publish(
+            aio_pika.Message(body=json.dumps(payload).encode()),
+            routing_key=queue.name
+        )
